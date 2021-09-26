2c1def4277c3350839f585df8a29c6ce43f89a52
Oren S.Rauch <orensrauch@gmail.com>
2021-09-26 13:39:23 +0300

    improve cookies transaction and elements cypress test.js

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -2,8 +2,12 @@
 describe("test with ci", () => {
     it("should visit automation test store website", () => {
         cy.visit("https://automationteststore.com/index.php?rt=account/create")
-        cy.url("https://automationteststore.com/index.php?rt=account/create").should('eq', 'https://automationteststore.com/index.php?rt=account/create')
+        cy.url("https://automationteststore.com/index.php?rt=account/create")
+        .should('eq', 'https://automationteststore.com/index.php?rt=account/create')
         cy.log('after verify url address')
+        cy.clearLocalStorageSnapshot()
+        cy.log('after cleaning local storage before tests run')
+
     })
 
     it("should fill form", () => {
@@ -18,8 +22,6 @@ describe("test with ci", () => {
         cy.log('FAX number')
         cy.get('#AccountFrm_fax').type('9876543210')
         cy.log('End of first form')
-    })
-    it("should fill Your Address checkout form", () => {
         cy.log('company')
         cy.get('#AccountFrm_company').type('hackermoon')
         cy.log('Address 1')
@@ -32,19 +34,16 @@ describe("test with ci", () => {
         cy.get('#AccountFrm_country_id').select('Zimbabwe').should('have.value', '239')
         cy.log('Region / state')
         cy.get('#AccountFrm_zone_id').select('Matabeleland North').should('have.value', '3839')
-
-    })
-    it('should fill Login Details', () => {
         cy.get('#AccountFrm_loginname').type('hackerman142')
         cy.get('#AccountFrm_password').type('0b9b8b7b6B5b')
         cy.get('#AccountFrm_confirm').type('0b9b8b7b6B5b')
-    })
-    it('should select No for Newsletter', () => {
         cy.get('[for="AccountFrm_newsletter0"]').click()
         cy.log('newsletter')
         cy.get('#AccountFrm_agree').click()
         cy.log('continue button')
         cy.get('.col-md-2 > .btn').click()
+        cy.saveLocalStorage()
+
     })
     it('Should navigate to main page', () => {
         cy.get('.logo > img').click()
@@ -54,13 +53,56 @@ describe("test with ci", () => {
         cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(3) > .fixed_wrapper > .fixed > .prdocutname').scrollIntoView()
         cy.wait(500)
     })
-    it("should add product to cart", () => {
+    it("should select and add product to cart", () => {
 
         cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(3) > .thumbnail > .pricetag > .productcart > .fa').click().wait(500)
-    })
-    it("should click on quick cart on product thumbnail", () => {
+        cy.saveLocalStorage()
+        cy.saveLocalStorage()
         cy.get('.quick_basket > a > .fa').click()
+        cy.saveLocalStorage()
         cy.wait(550)
+    })
+
+    it("should validate product table content", () => {
+        cy.get('.product-list > table > tbody > tr').should(($tr) => {
+            // should have found 2 table row elements
+            expect($tr).to.have.length(2)
+
+            expect($tr.eq(0), 'First table row')
+            .to.contain('Image')
+            .to.contain('Name')
+            .to.contain('Model')
+            .to.contain('Unit Price')
+
+            expect($tr.eq(1), 'Second table row')
+            .to.contain('a')
+            .to.contain('Benefit Bella Bamba')
+            .to.contain('523755')
+            .to.contain('$56.00')
+
+   
+        })
+
+        cy.log('after tr column content validate')
+
+        cy.get('.product-list > .table > tbody > tr > td').should(($td) => {
+            const classes = $td.map((i, el) => {
+                return Cypress.$(el).attr('class')
+            })
+          
+            // call classes.get() to make this a plain array
+            expect(classes.get()).to.deep.eq([
+                "align_center",
+                "align_left",
+                "align_left",
+                "align_right",
+                "align_center",
+                "align_right",
+                "align_center",
+            ])
+        })
+        cy.log('after tr classes validate')
+
     })
 
 })
